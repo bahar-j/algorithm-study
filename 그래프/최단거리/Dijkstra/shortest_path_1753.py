1d079eac0472bfd12a24296db3348c249623fb31
jooeun <joonie811@naver.com>
2020-09-13 07:46:18 +0900

    Change name

---
+++ 그래프/최단거리/Dijkstra/shortest_path_1753.py
@@ -0,0 +1,29 @@
+from sys import stdin
+import heapq
+
+V, E = map(int, stdin.readline().split())
+K = int(stdin.readline())
+G = {i: [] for i in range(1, V+1)}
+
+for _ in range(E):
+    u, v, w = map(int, stdin.readline().split())
+    G[u].append([v, w])
+
+
+dist = [float('inf')] * (V+1)  # 주어진 정점으로부터의 거리
+dist[K] = 0
+queue = list()  # min-heap
+queue.append([dist[K], K])  # (weight, vertex), weight 기준 정렬
+while queue:
+    u = heapq.heappop(queue)[1]
+    for v, w in G[u]:  # u의 모든 인접 정점 relax
+        if dist[v] > dist[u] + w:
+            dist[v] = dist[u] + w
+            heapq.heappush(queue, [dist[v], v])
+    # print(queue)
+for i in range(1, V+1):
+    if dist[i] == float('inf'):
+        print("INF")
+    else:
+        print(dist[i])
+
---
+++ 그래프/최단거리/floidwarshall_11404.py
@@ -0,0 +1,33 @@
+from sys import stdin
+
+N = int(stdin.readline().strip())
+M = int(stdin.readline().strip())
+INF = float('inf')
+
+G = [[INF]*(N+1) for _ in range(N+1)]
+
+# 자기 자신으로 가는 경우
+for i in range(1, N+1):
+    G[i][i] = 0
+
+# 모든 노드에서 인접 노드로 가는 비용 setting
+for _ in range(M):
+    a, b, cost = map(int, stdin.readline().split())
+    if G[a][b] > cost:
+        G[a][b] = cost
+
+# (모든 경유지 k에 대해서) i->j로 경유지 k를 거쳐서 가는 방법
+for k in range(1, N+1):
+    for i in range(1, N+1):
+        for j in range(1, N+1):
+            if G[i][j] > G[i][k] + G[k][j]:
+                G[i][j] = G[i][k] + G[k][j]
+
+# 출력
+for i in range(1, N+1):
+    for j in range(1, N+1):
+        if G[i][j] == INF:
+            print(0, end=" ")
+        else:
+            print(G[i][j], end=" ")
+    print()
