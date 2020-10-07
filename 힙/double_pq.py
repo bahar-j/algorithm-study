2194e0f015ae7bbc9bbd916b80f53e072ee36bb2
jooeun <joonie811@naver.com>
2020-10-07 15:46:44 +0900

    heap

---
+++ 힙/double_pq.py
@@ -0,0 +1,28 @@
+import heapq
+
+def solution(operations):
+    answer = []
+    queue = []
+    idx = 0
+
+    while idx < len(operations):
+        tmp = operations[idx].split()
+        cmd = tmp[0]
+        data = int(tmp[1])
+        if cmd == 'I':
+            heapq.heappush(queue, data)
+        else:
+            if queue:
+                if data == -1:
+                    heapq.heappop(queue)
+                else:
+                    queue.pop(queue.index(max(queue)))
+        idx += 1
+
+    if not queue:
+        return [0, 0]
+
+    answer.append(max(queue))
+    answer.append(queue[0])
+
+    return answer
