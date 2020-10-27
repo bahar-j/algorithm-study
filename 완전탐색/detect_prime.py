4a333471f1d44cc9b806262a9487204ef36219df
jooeun <joonie811@naver.com>
2020-10-28 03:31:32 +0900

    정렬 브루트포스 추가

---
+++ 완전탐색/detect_prime.py
@@ -0,0 +1,32 @@
+from itertools import permutations
+
+
+def is_prime(num):
+    is_prime = True
+    if num > 1:
+        for i in range(2, num):
+            if num % i == 0:
+                is_prime = False
+                break
+    else:
+        is_prime = False
+    return (is_prime)
+
+
+def solution(numbers):
+    answer = 0
+    prime_nums = list()
+
+    # 자릿수 별 순열
+    for j in range(1, len(numbers) + 1):
+        p_list = list(map(''.join, permutations(numbers, j)))
+
+        # 소수인지 확인
+        for i in range(len(p_list)):
+            if int(p_list[i]) not in prime_nums:
+                if is_prime(int(p_list[i])):
+                    prime_nums.append(int(p_list[i]))
+
+    answer = len(prime_nums)
+
+    return answer
---
+++ 정렬/h-index.py
@@ -0,0 +1,10 @@
+def solution(citations):
+    answer = 0
+    citations.sort(reverse=True)
+    # i == 체크할 index , i+1 == 인용 횟수
+    for i in range(len(citations) - 1, -1, -1):
+        if citations[i] - (i + 1) >= 0:
+            answer = i + 1
+            break
+
+    return answer
